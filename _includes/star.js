ede4c4919aa2cf970fa07064a46887d726643fbf
Revned <revned@gmail.com>
2016-05-07 14:28:14 -0600

    Fix slight shift when stars are added

--- _includes/star.js
+++ _includes/star.js
@@ -1,7 +1,7 @@
       var now = new Date().getTime();
-      e = document.querySelectorAll(".new");
+      e = document.querySelectorAll(".map li:last-child");
       for(i = 0; i < e.length; i++) {
-        if (now - Date.parse(e[i].parentNode.textContent) < 15768000000) {
-          e[i].innerHTML = "★";
+        if (now - Date.parse(e[i].textContent) < 15768000000) {
+          e[i].parentNode.parentNode.className += " new";
         }
       }
