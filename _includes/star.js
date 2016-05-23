8fec00bd4de649d36219078288a73a128f03fd19
Revned <revned@gmail.com>
2016-05-23 00:30:06 -0600

    Simplify file structure

--- _includes/star.js
+++
@@ -1,7 +0,0 @@
-      var now = new Date().getTime();
-      e = document.querySelectorAll(".map li:last-child");
-      for(i = 0; i < e.length; i++) {
-        if (now - Date.parse(e[i].textContent) < 15768000000) {
-          e[i].parentNode.parentNode.className += " new";
-        }
-      }
